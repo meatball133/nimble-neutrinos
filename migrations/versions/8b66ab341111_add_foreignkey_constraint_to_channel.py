7ea0360f6c2777a2f348e15c9bea8cb686e1fcd3
meatball <meatballsofmeatballs@gmail.com>
2024-07-22 12:28:15 +0200

    `alembic revision -m "add foreignkey constraint to channel"`

---
+++ migrations/versions/8b66ab341111_add_foreignkey_constraint_to_channel.py
@@ -0,0 +1,26 @@
+"""add foreignkey constraint to channel
+
+Revision ID: 8b66ab341111
+Revises: 64a7c9627e6c
+Create Date: 2024-07-22 12:28:03.537042
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '8b66ab341111'
+down_revision: Union[str, None] = '64a7c9627e6c'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
