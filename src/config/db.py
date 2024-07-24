c5772cc3bb9e10c23a6db1fedb2a8d2c843a3bd2
malik1004x <malikowskirobert@gmail.com>
2024-07-24 21:02:28 +0200

    feat: config via environment

--- src/config/db.py
+++ src/config/db.py
@@ -1,16 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, DeclarativeBase
 from sqlalchemy.engine.url import URL
+from os import getenv
+
 
 class Base(DeclarativeBase):
     pass
 
+
 url = URL.create(
     "postgresql",
-    username="postgres",
-    password="postgres",
-    host="localhost",
-    database="nimble"
+    username=getenv("POSTGRES_USERNAME"),
+    password=getenv("POSTGRES_PASSWORD"),
+    host=getenv("POSTGRES_HOST"),
+    database=getenv("POSTGRES_DBNAME"),
 )
 engine = create_engine(url, echo=True)
 
